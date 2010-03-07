d941289628aa271d9015d709290dcc9ef5d6395f
Tomas Doran <bobtfish@bobtfish.net>
2010-03-07 21:05:57 +0000

    Move actions out of test app

--- t/lib/AuthDigestTestApp.pm
+++ t/lib/AuthDigestTestApp.pm
@@ -1,26 +1,23 @@
 package AuthDigestTestApp;
-    use Catalyst qw/
-      Authentication
-      Cache
-      /;
-    
-    our %users;
-    sub moose : Local {
-        my ( $self, $c ) = @_;
-        #$c->authenticate( { realm => 'testrealm@host.com' } );
-        $c->authenticate();
-        $c->res->body( $c->user->id );
-    }
-    my $digest_pass = Digest::MD5->new;
-    $digest_pass->add('Mufasa2:testrealm@host.com:Circle Of Life');
-    %users = ( 
-        Mufasa  => { pass         => "Circle Of Life",          }, 
+use Catalyst qw/
+    Authentication
+    Cache
+/;
+
+our %users;
+my $digest_pass = Digest::MD5->new;
+$digest_pass->add('Mufasa2:testrealm@host.com:Circle Of Life');
+%users = (
+        Mufasa  => { pass         => "Circle Of Life",          },
         Mufasa2 => { pass         => $digest_pass->hexdigest, },
-    );
-    __PACKAGE__->config->{cache}{backend} = {
-        class => 'Cache::FileCache',
-    };
-    __PACKAGE__->config( authentication => {
+);
+__PACKAGE__->config(
+    cache => {
+        backend => {
+            class => 'Cache::FileCache',
+        },
+    },
+    authentication => {
         default_realm => 'testrealm@host.com',
         realms => {
             'testrealm@host.com' => {
@@ -31,10 +28,14 @@ package AuthDigestTestApp;
                 credential => {
                     class => 'HTTP',
                     type  => 'digest',
-                    password_type => 'clear', 
+                    password_type => 'clear',
                     password_field => 'pass'
                 },
             },
         },
-    });
-    __PACKAGE__->setup;
+    },
+);
+__PACKAGE__->setup;
+
+1;
+
--- t/lib/AuthTestApp.pm
+++ t/lib/AuthTestApp.pm
@@ -1,34 +1,29 @@
 package AuthTestApp;
-    use Catalyst qw/
-      Authentication
-      /;
-    our %users;
-    __PACKAGE__->config(authentication => {
+use Catalyst qw/
+    Authentication
+/;
+our %users;
+__PACKAGE__->config(
+    'Plugin::Authentication' => {
         default_realm => 'test',
-        realms => {
-            test => {
-                store => { 
-                    class => 'Minimal',
-                    users => \%users,
-                },
-                credential => { 
-                    class => 'HTTP', 
-                    type  => 'basic',
-                    password_type => 'clear', 
-                    password_field => 'password'
-                },
+        test => {
+            store => {
+                class => 'Minimal',
+                users => \%users,
+            },
+            credential => {
+                class => 'HTTP',
+                type  => 'basic',
+                password_type => 'clear',
+                password_field => 'password'
             },
         },
-    });
-    sub auto : Private {
-        my ($self, $c) = @_;
-        $c->authenticate();
-    }
-    sub moose : Local {
-        my ( $self, $c ) = @_;
-	    $c->res->body( $c->user->id );
-    }
-    %users = (
-        foo => { password         => "s3cr3t", },
-    );
-    __PACKAGE__->setup;
+    },
+);
+%users = (
+    foo => { password         => "s3cr3t", },
+);
+__PACKAGE__->setup;
+
+1;
+
